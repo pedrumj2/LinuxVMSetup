138da2afb9e4d2236df095e5d9507f808d172eb6
pedrum <pedrum.jalali@gmail.com>
2017-02-09 13:09:31 -0500

    finished upto gdrive

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-
+#
 #eval "apt-get update"
-
-#eval "service mysql stop"
-#eval "killall -KILL mysql mysqld_safe mysqld"
-#eval "apt-get --yes purge mysql-server mysql-client"
-#eval "apt-get --yes autoremove --purge"
-#eval "apt-get autoclean"
-
+#
+##eval "service mysql stop"
+##eval "killall -KILL mysql mysqld_safe mysqld"
+##eval "apt-get --yes purge mysql-server mysql-client"
+##eval "apt-get --yes autoremove --purge"
+##eval "apt-get autoclean"
+#
 #export DEBIAN_FRONTEND="noninteractive"
-
+#
 #eval "debconf-set-selections <<< \"mysql-server mysql-server/root_password password fafdRE\\\$3\""
 #eval "debconf-set-selections <<< \"mysql-server mysql-server/root_password_again password fafdRE\\\$3\""
-
+#
 #eval "apt-get install -y mysql-server"
-
-
+#
+#
 #eval "mysql -u root -p'fafdRE\$3' -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.24.%.%' IDENTIFIED BY 'fafdRE\\\$3' WITH GRANT OPTION;FLUSH PRIVILEGES;\""
 #eval "mysql -u root -p'fafdRE\$3' -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'141.117.%.%' IDENTIFIED BY 'fafdRE\\\$3' WITH GRANT OPTION;FLUSH PRIVILEGES;\""
-                    
-
+#                    
+#
 #if [ -a "/etc/mysql/mysql.conf.d/mysqld.cnf" ]
 #then
 #  eval "sed -i -e '0,/\[mysqld\]/s/\[mysqld\]/[mysqld]\ninnodb_buffer_pool_size=30G\ninnodb_buffer_pool_instances=4/g' /etc/mysql/mysql.conf.d/mysqld.cnf"
@@ -29,15 +29,27 @@
 #  eval "sed -i -e '0,/\[mysqld\]/s/\[mysqld\]/[mysqld]\ninnodb_buffer_pool_size=2G\ninnodb_buffer_pool_instances=4/g' /etc/mysql/my.cnf"
 #  eval "sed -i -e '0,/bind-address.*/s/bind-address.*/#bind-address     127.0.0.1/g' /etc/mysql/my.cnf"
 #fi
-
-
-
-
-
+#
+#
+#
+#
+#
 #eval "service mysql restart "
+#
+#
+#
+#eval "sudo apt -y install gcc"
+#eval "sudo apt -y install gdb"
 
+#eval "wget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download"
+#eval "mv uc\?id\=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA gdrive"
+#eval "chmod +x gdrive"
+#eval "sudo install gdrive /usr/local/bin/gdrive"
+#eval "rm gdrive"
+#( cd ..
+#eval "git clone git@github.com:pedrumj2/Pcap_extractor"
+  #eval "git clone git@github.com:pedrumj2/XML2CSV"
+  #eval "git clone git@github.com:pedrumj2/net_header_database"
 
-
-eval "sudo apt -y install gcc"
-eval "sudo apt -y install gdb"
+#)
 
