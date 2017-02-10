214a942d111c885aa01ddf43ae1693d2df21cef7
pedrum <pedrum.jalali@gmail.com>
2017-02-09 21:47:53 -0500

    f

--- setup.sh
+++ setup.sh
@@ -41,15 +41,22 @@
 #eval "sudo apt -y install gcc"
 #eval "sudo apt -y install gdb"
 
+#eval "sudo apt-get -y install unzip"
+#eval "sudo apt-get -y install xsltproc"
+
 #eval "wget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download"
-#eval "mv uc\?id\=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA gdrive"
+  #eval "mv uc\?id\=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA gdrive"
 #eval "chmod +x gdrive"
 #eval "sudo install gdrive /usr/local/bin/gdrive"
 #eval "rm gdrive"
-#( cd ..
+( cd ..
 #eval "git clone git@github.com:pedrumj2/Pcap_extractor"
   #eval "git clone git@github.com:pedrumj2/XML2CSV"
   #eval "git clone git@github.com:pedrumj2/net_header_database"
-
-#)
+  #eval "chown -R sjalalim:   XML2CSV"
+  #eval "chown -R sjalalim:   net_header_database"
+  #eval "git clone git@github.com:pedrumj2/mysql_csv_import"
+  #eval "chown -R sjalalim:   mysql_csv_import"
+ 
+)
 
